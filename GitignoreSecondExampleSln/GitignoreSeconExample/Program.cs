f8e410912a1cef24ca67175377fda38e1c911723
Yamkela24 <151846985+Yamkela24@users.noreply.github.com>
2023-11-30 13:33:05 +0200

    initial commit

---
+++ GitignoreSecondExampleSln/GitignoreSeconExample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitignoreSeconExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
