3139d3b9226f8eeea8f89f4540be86fcd8b01a1e
Yamkela24 <151846985+Yamkela24@users.noreply.github.com>
2023-11-30 13:48:36 +0200

    Added for loop to Hello World

--- GitignoreSecondExampleSln/GitignoreSeconExample/Program.cs
+++ GitignoreSecondExampleSln/GitignoreSeconExample/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(" Hello, World");
+
+                Console.WriteLine("Goodbye, World!");
+            }
+
+            
         }
     }
 }
